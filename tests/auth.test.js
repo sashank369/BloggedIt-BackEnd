052509c47b1bd55da5e312d7bee19b282c68bf89
Rithvik99 <rithvikramasani@gmail.com>
2023-12-14 03:10:46 +0530

    Final Fix

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -19,7 +19,6 @@ describe('Test wrong login password', () => {
   });
 });
 
-
 describe('Test Already Existing User', () => {
   it('returns an error if the user already exists', async () => {
     const response = await request(app)
